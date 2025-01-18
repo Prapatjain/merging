c66dcb55eef452a01aff393a873e873625c47fbf
Prapatjain <prapatjain@gmail.com>
2025-01-18 13:51:04 +0530

    sub file added to subon

---
+++ substraction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class substraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println(" ");
+        System.out.println("Substracton");
+        System.out.println("enter two values:");
+        int n =sc.nextInt();
+        int m =sc.nextInt();
+        System.out.println(m-n);
+    }
+}
